b2b02ab8600416ff300cce5a95ad3aaf5aba7193
Ruben E Acosta <eldragondera1@gmail.com>
2021-01-12 17:44:09 -0500

    react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { ListCategories } from './components/ListCategories'
 import { GlobaStyles } from './GlobalStyles'
+import { ListCategories } from './components/ListCategories'
+import { ListPhotoCards } from './components/ListPhotoCards'
 
 export const App = () => {
   return (
     <>
       <GlobaStyles />
       <ListCategories />
+      <ListPhotoCards />
     </>
   )
 }
--- src/components/ListCategories/styles.js
+++ src/components/ListCategories/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const List = styled.ul`
   display: flex;
+  margin-bottom: 20px;
   overflow: scroll;
   width: 100%;
 `
