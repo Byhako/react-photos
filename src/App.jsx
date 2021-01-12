052cc2f24a2bc1304dcae612518d8def491a52cf
Ruben E Acosta <eldragondera1@gmail.com>
2021-01-12 14:52:40 -0500

    styles components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ListCategories } from './components/ListCategories'
+import { GlobaStyles } from './GlobalStyles'
+
+export const App = () => {
+  return (
+    <>
+      <GlobaStyles />
+      <ListCategories />
+    </>
+  )
+}
