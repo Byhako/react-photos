1bf96bf2c4b5d14aaac3c2135062e8954ff16a83
Ruben E Acosta <eldragondera1@gmail.com>
2021-01-12 23:36:55 -0500

    logo

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## curso-platzi-react-avanzado ⚛️
 
 [Curso Avanzado de React de Platzi](https://platzi.com/clases/react-avanzado/)
+
+https://maketext.io
+https://jakearchibald.github.io/svgomg/
+https://react-svgr.com/playground/
--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { GlobaStyles } from './GlobalStyles'
 import { ListCategories } from './components/ListCategories'
 import { ListPhotoCards } from './components/ListPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => {
   return (
     <>
       <GlobaStyles />
+      <Logo />
       <ListCategories />
       <ListPhotoCards />
     </>
