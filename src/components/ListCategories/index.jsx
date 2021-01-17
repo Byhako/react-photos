ed836ea2872eb2b4d73136f45b2496fc33214c8e
Ruben E Acosta <eldragondera1@gmail.com>
2021-01-16 22:42:38 -0500

    barra fija

--- src/components/ListCategories/index.jsx
+++ src/components/ListCategories/index.jsx
@@ -4,20 +4,37 @@ import { List, Item } from './styles'
 
 export const ListCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     window.fetch('https://petgram-api-ruben.vercel.app/categories')
       .then(res => res.json())
       .then(response => setCategories(response))
+
+    const onScroll = () => {
+      const newShowFixed = window.scrollY > 200
+      setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll')
   }, [])
 
-  return (
-    <List>
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {categories.map(category => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
