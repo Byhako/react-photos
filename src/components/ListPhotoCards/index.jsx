85cee05e7d4c4b32f420a3c4e8b48fef8afa8964
Ruben E Acosta <eldragondera1@gmail.com>
2021-01-27 15:12:17 -0500

    iniciando apollo

--- src/components/ListPhotoCards/index.jsx
+++ src/components/ListPhotoCards/index.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
-export const ListPhotoCards = () => {
+const withPhotos = graphql(gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`)
+
+const ListPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7, 8].map(id => (
-        <li key={id}>
-          <PhotoCard id={id} />
+      {photos.map(photo => (
+        <li key={photo.id}>
+          <PhotoCard id={photo.id} {...photo} />
         </li>
       ))}
     </ul>
   )
 }
+
+export const ListPhotoCards = withPhotos(ListPhotoCardsComponent)
