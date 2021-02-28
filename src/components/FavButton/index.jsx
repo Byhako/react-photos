7deb632341a887fa4ca364fa474f9ec57430e95d
Ruben E Acosta <eldragondera1@gmail.com>
2021-02-27 21:43:12 -0500

    propTypes

--- src/components/FavButton/index.jsx
+++ src/components/FavButton/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { Button } from '../PhotoCard/styles'
+import PropTypes from 'prop-types'
 
 export const FavButton = ({ liked, likes, onClick }) => {
   const Icon = liked ? MdFavorite : MdFavoriteBorder
@@ -10,4 +11,10 @@ export const FavButton = ({ liked, likes, onClick }) => {
       <Icon size='24px' /> {likes} likes!
     </Button>
   )
+}
+
+FavButton.propTypes = {
+  liked: PropTypes.bool.isRequired,
+  likes: PropTypes.number,
+  onClick: PropTypes.func
 }
